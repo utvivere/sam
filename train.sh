7ea9b3598d1b043bf4bbde5b24b9b2306dd84c7e
數心 <47732475+Matheart@users.noreply.github.com>
2024-06-03 20:58:46 +0200

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,14 +1,14 @@
 cd example
 # varies rho
-python train.py --adaptive false --rho 0.01
-python train.py --adaptive false --rho 0.02
-python train.py --adaptive false --rho 0.05
-python train.py --adaptive false --rho 0.1
-python train.py --adaptive false --rho 0.2
-python train.py --adaptive false --rho 0.5
+python train.py --rho 0.01
+python train.py --rho 0.02
+python train.py --rho 0.05
+python train.py --rho 0.1
+python train.py --rho 0.2
+python train.py --rho 0.5
 
 # compare adaptive and non-adaptive
-python train.py --adaptive false
-python train.py --adaptive true
+python train.py 
+python train.py --adaptive --rho 2
 
 # different norm, L2 and L_infty
